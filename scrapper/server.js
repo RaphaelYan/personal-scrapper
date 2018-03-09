dce8c8de65f80c4f0e50b69d65c9c63efb573de6
Nsy <noelie@donkeycode.com>
2018-03-09 15:42:09 +0100

    scrapper & account key

--- scrapper/server.js
+++ scrapper/server.js
@@ -1,43 +1,101 @@
-var express = require('express');
-var fs = require('fs');
-var request = require('request');
-var cheerio = require('cheerio');
-var firebase = require('firebase');
-var app     = express();
+var express       = require('express');
+var fs            = require('fs');
+var request       = require('request');
+var cheerio       = require('cheerio');
+var admin         = require('firebase-admin');
 
-firebase.initializeApp({
-  apiKey: "AIzaSyAg8XBoLICqKK1uPqD6BXgtBb3jswhq7Y8",
-  authDomain: "personal-scrapper.firebaseapp.com",
+var app           = express();
+const bodyParser  = require('body-parser');
+
+var serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
   databaseURL: "https://personal-scrapper.firebaseio.com",
-  projectId: "personal-scrapper",
-  storageBucket: "personal-scrapper.appspot.com",
-  messagingSenderId: "418768715450"
+  credential: admin.credential.cert(serviceAccount),
 });
 
-app.get('/scrape', function(req, res) {
-    var url = 'https://www.youtube.com/user/joueurdugrenier/videos';
+app.use(bodyParser.json());
 
+function addItem(user, item) {
+  if (!user.items.find(i => i.id === item.id)) {
+    admin.firestore().collection('items').add({
+      id: item.id,
+      title: item.title,
+      userid: user.uid,
+    });
+  } else {
+    console.log("on add pas: existe deja");
+  }
+}
+
+function scrap(url, user) {
+  return new Promise((resolve, reject) => {
     request(url, function(error, response, html) {
-        if (!error){
-        	console.log('cool');
+      if (!error) {
+        var $ = cheerio.load(html);
 
-            var $ = cheerio.load(html);
+        $('.channels-content-item').filter(function() {
+            var data = $(this);
+            var title = data.find('.yt-lockup-title').children().first().text();
+            var id = data.find('.yt-lockup-video').attr('data-context-item-id');
+            addItem(user, {title, userid : user.uid, id: id });
+        });
+        resolve();
+      } else {
+        reject(console.log(error));
+      }
+    });
+  });
+}
 
-            $('.yt-lockup-title').filter(function() {
-                var data = $(this);
-                var title = data.children().first().text();
-                console.log('pwet', title);
-                firebase.database().ref('/items').push({
-                  title: title,
-                });
-            });
+app.post('/scrape', function(req, res) {
+  console.log(req.body);
+  let userid = req.body.userid;
+  let url = req.body.url;
+  let user = null;
 
-        } else {
-        	console.log(error);
-        }
-	    res.send(html);
-    })
+  console.log('user ' + userid + ' scrapping ' + url);
+
+  admin.auth().getUser(userid).then(function(userRes) {
+    console.log("Successfully fetched user data:", userRes.toJSON());
+    user = userRes;
+
+    user.items = [];
+    admin.firestore().collection('items').where("userid", "==", userid).get().then((querySnapshot) => {
+
+      querySnapshot.forEach(function(doc) {
+        console.log(doc.id, " => ", doc.data());
+        user.items.push(doc.data());
+      });
+      console.log("after");
+      console.log(user.items);
+
+      scrap(url, user).then(() => {
+        res.send('blah');
+      });
+    }).catch(function(error) {
+      console.log("Error fetching user data:", error);
+    });
+
+  });
 })
+
+app.get('/scrape', function(req, res) {
+
+  request("https://www.youtube.com/user/joueurdugrenier/videos", function(error, response, html) {
+    if (!error) {
+      var $ = cheerio.load(html);
+      $('.yt-lockup-title').filter(function() {
+          var data = $(this);
+          var title = data.children().first().text();
+          console.log('pwet', title);
+      });
+      res.send(html);
+    } else {
+      console.log(error);
+    }
+  });
+});
 
 app.listen('8081')
 console.log('Magic happens on port 8081');
