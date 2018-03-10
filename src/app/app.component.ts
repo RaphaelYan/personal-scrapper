a5af73a195de5f9b814b211f2d86e00a3bdfbcb4
Raphael Yan <raphael@donkeycode.com>
2018-03-10 10:11:41 +0100

    bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ interface Site {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   private itemsCollection: AngularFirestoreCollection<Item>;
