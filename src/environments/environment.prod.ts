47518cc23a7740fa952a7f853a5e2648660ebde2
Raphael Yan <raphael@donkeycode.com>
2018-03-10 17:43:50 +0100

    conf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAg8XBoLICqKK1uPqD6BXgtBb3jswhq7Y8',
+    authDomain: 'personal-scrapper.firebaseapp.com',
+    databaseURL: 'https://personal-scrapper.firebaseio.com',
+    projectId: 'personal-scrapper',
+    storageBucket: 'personal-scrapper.appspot.com',
+    messagingSenderId: '418768715450'
+  }
 };
